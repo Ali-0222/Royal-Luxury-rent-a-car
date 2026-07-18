11fb981345c9a1d35d8e66b406cdf04c677260da
Muhammad Sakhawat Ali <132989770+Ali-0222@users.noreply.github.com>
2026-07-18 23:39:45 +0500

    Update office address in contact.ts

--- app/data/contact.ts
+++ app/data/contact.ts
@@ -4,5 +4,5 @@ export const CONTACT_EMAIL = 'info@royalluxuryrentacar.com';
 export const WHATSAPP_LINK = 'https://wa.me/923368018945?text=I%20want%20to%20rent%20a%20luxury%20car%20from%20Royal%20Luxury%20Rent%20a%20Car.';
 export const PHONE_NUMBER = '03368018945';
 export const PHONE_NUMBERS = '03342428554';
-export const OFFICE_ADDRESS = 'Office #3, Floor #3, Corporate Centre, Block B, Gulberg Greens, Islamabad';
+export const OFFICE_ADDRESS = 'House no 57, Halal Road F11, Islamabad ';
 
