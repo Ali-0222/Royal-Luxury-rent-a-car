fdfa8407e6bc1305a4572f9f293aff092d8c4732
Muhammad Sakhawat Ali <132989770+Ali-0222@users.noreply.github.com>
2026-07-19 14:33:31 +0500

    Comment out HomeVideo component
    Comment out the HomeVideo component in the page.

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function HomePage() {
         whatsappLink={WHATSAPP_LINK}
       />
 
-      <HomeVideo />
+      {/* <HomeVideo /> */}
 
       <FleetSection
         fleetCards={fleet.map((car) => (
